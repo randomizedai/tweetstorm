fb1daf9df11f16a43a019d1acbb938378a1bb9c6
amitgupta151 <amitgupta151@gmail.com>
2014-09-20 11:10:42 +0200

    url hdfs

--- compute_features.py
+++ compute_features.py
@@ -12,17 +12,21 @@ def compute_feature(feature_row,file_row,worker_id,debug=False):
     filename = get_filename_from_path(input_file_path)
     filename_suffix = filename[len(feature_row['input_feature']):]
     output_filename = feature_row['output_feature'] + filename_suffix
-    output_file_path = os.path.dirname(input_file_path) + "/" + output_filename
-    tempval = get_random_uuid()
-    temp_output_file_path = os.path.dirname(input_file_path) + "/_tmp_" + tempval
-    feature_command = feature_row['command'] + " < " + input_file_path + " > " + temp_output_file_path
+    output_file_path = ""
+    feature_command = feature_row['command'] + " < " + input_file_path
+    if output_filename:
+        output_file_path = os.path.dirname(input_file_path) + "/" + output_filename
+        tempval = get_random_uuid()
+        temp_output_file_path = os.path.dirname(input_file_path) + "/_tmp_" + tempval
+        feature_command = feature_command + " > " + temp_output_file_path
     if debug:
-        print "taskid--" + str(worker_id) +  "Feature command --> " + feature_command
-        print "taskid--" + "mv " + temp_output_file_path + " " + output_file_path
+        print "taskid-- " + str(worker_id) +  "Feature command --> " + feature_command
+        print "taskid-- " + "mv " + temp_output_file_path + " " + output_file_path
     
     try:
         p1  = subprocess.check_call(feature_command,shell=True)
-        p2 = subprocess.check_call("mv " + temp_output_file_path + " " + output_file_path,shell=True )
+        if output_filename:
+            p2 = subprocess.check_call("mv " + temp_output_file_path + " " + output_file_path,shell=True )
         return ("success",(output_file_path,output_filename))
     except Exception, e:
         return ("exception",e)
---
+++ features/mvurlhdfs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hadoop fs -put -  /user/hdfs/data_processing/data_collection/urls/to_download/
