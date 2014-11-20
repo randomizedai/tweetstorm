80aeeefbc930638794b14246d878961f552c41bf
toluolll <toluolll@gmail.com>
2014-11-20 17:28:57 +0100

    Small adoptation for synonyms detection

--- features/utils/synonyms_utils.py
+++ features/utils/synonyms_utils.py
@@ -13,7 +13,7 @@ def jaccard(t1, t2):
 	disjunction_score = sum(t1.values()) + sum(t2.values())
 	return float(intersection_score) / disjunction_score
 """
-topic - topic_norm_name : {"concepts": [(concept_i, concept_norm_name_i, weight_i)] }
+[topic_name : {'norm_name': topic_norm_name; "concepts": [(concept_i, concept_norm_name_i, weight_i)] }]
 """
 def similarity_between_vectors(topic1, topic2):
 	t1 = {}
@@ -41,10 +41,30 @@ def similarity_between_each_vectors(topics):
 
 if __name__ == "__main__":
 	topics = []
-	for row in sys.stdin:
-		try:
-			topic = json.loads(row)
-		except Exception, e:
-			continue
-		topics.append(topic)
-	print similarity_between_each_vectors(topics)
+	# for row in sys.stdin:
+	# 	try:
+	# 		topic = json.loads(row)
+	# 	except Exception, e:
+	# 		continue
+	# 	topics.append(topic)
+
+	# topics here are: {topic_norm_name: {con_norm_name:[weight, con_name]}}
+	map_, _, top = read_topic_to_json_from_db(path='http://146.148.70.53/topics/list/?page_size=1000&concepts=1', dir_maps=BASE_DIR+'/../data/')
+	for k, v in top.items():
+		topic_name = map_[k][0]
+		topic = {}
+		topic[topic_name] = {}
+		topic[topic_name]['norm_name'] = k
+		topic[topic_name]['concepts'] = []
+		for k_sub, v_sub in v.items():
+			topic[topic_name]['concepts'].append((v_sub[1], k_sub, v_sub[0]))
+		if topic[topic_name]['concepts']:
+			topics.append(topic)
+
+	similarity_scores = similarity_between_each_vectors(topics)
+	print similarity_scores[0:10]
+
+
+
+
+
